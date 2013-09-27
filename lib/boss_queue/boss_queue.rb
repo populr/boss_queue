8ff35df4ca66d9b8178b605886ce1beb461b4042
Daniel Nelson <dnelson@centresource.com>
2013-09-27 00:21:03 -0500

    class method

--- lib/boss_queue/boss_queue.rb
+++ lib/boss_queue/boss_queue.rb
@@ -29,7 +29,7 @@ class BossQueue
 
   def self.create_table(read_capacity=1, write_capacity=1, options={})
     create_opts = {}
-    create_opts[:hash_key] = { hash_key => :string }
+    create_opts[:hash_key] = { self.hash_key => :string }
     create_opts[:range_key] = { :kind => :string }
 
     AWS::DynamoDB.new.tables.create(self.table_name, read_capacity, write_capacity, create_opts)
